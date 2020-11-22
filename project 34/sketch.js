1bcdd423425a7ef440877e9b6e0821f0bae18a79
Prakhar359 <72444330+Prakhar359@users.noreply.github.com>
2020-11-22 22:09:29 +0530

    Add files via upload

---
+++ project 34/sketch.js
@@ -0,0 +1,23 @@
+var dog1;
+ var database,position;
+
+function preload()
+{
+loadImage=dog1;
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
